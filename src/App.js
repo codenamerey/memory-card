d6dd9b4589d5988dc006521f365b76a8accb8f6d
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-08 17:41:55 +0800

    Fetch Card GIFs depending on App request

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import Card from "./components/Card";
 import { useEffect, useState } from "react";
 
 function App() {
+  const characters = ["Harry Potter", 'Albus Dumbledore', 'Bella Lestrange', 'Hermione Granger', 'Ron Weasley', 'Draco Malfoy', 'Voldemort']
   const [randomCharacters, setRandomCharacters] = useState([]);
 
   useEffect(()=> {
     document.title = 'Memory Game';
+    pickRandomCharacters(characters, 4);
+    console.log(randomCharacters);
   }, []);
 
   useEffect(() => {
 
   }, [randomCharacters])
 
+
+  function pickRandomCharacters(characters, numberOfCharacters) {
+    let shallowCharacters = characters.slice();
+    let functionRandomCharacters = [];
+    for(let i=numberOfCharacters; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * shallowCharacters.length);
+      let randomCharacter = shallowCharacters.splice(randomIndex, 1);
+      console.log(randomCharacter);
+      functionRandomCharacters.push(randomCharacter[0]);
+    }
+    console.log(functionRandomCharacters);
+    setRandomCharacters(functionRandomCharacters);
+  }
+
   return (
     <div className="App">
       <Header></Header>
-      <Card></Card>
+      {
+        randomCharacters.map(randomCharacter => {
+          return <Card character={randomCharacter}></Card>
+        })
+      }
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react';
 import './Card.css';
 
-function Card() {
+
+function Card(props) {
+    const { character } = props;
+    const [gifURL, setGifURL] = useState('');
+    useEffect(()=> {
+        const URL = `https://api.giphy.com/v1/gifs/translate?api_key=z4L4W8mw0SgWTkLcbb9BIkykMbaZ7hvf&s=${convertToSearchable(character)}`;
+        setGIF(URL);
+    }, []);
+
+    function convertToSearchable(searchTerm) {
+        let searchTermLowercased = searchTerm.toLowerCase();
+        let spaceReplacedWithPlus = searchTermLowercased.replace(' ', '+');
+        return spaceReplacedWithPlus;
+    }
+
+    async function setGIF(url) {
+        try {
+            const response = await fetch(url);
+            const gifData = await response.json();
+            setGifURL(gifData.data.images.original.url); 
+        }
+        catch {
+            setGifURL('There has been a problem loading this image');
+        }
+    }
+
     return (
         <div className="card">
-            <img src="https://media4.giphy.com/media/d6Ni9aqSatPfq/giphy.gif?cid=609307femwj1tc3mwpz3lel0di7n33ckx5ic2279udlyrepf&rid=giphy.gif&ct=g" alt="Harry Potter" />
+            <img src={gifURL} alt="Harry Potter" />
             <div className="description">
-                <p>Harry Potter</p>
+                <p>{character}</p>
             </div>
         </div>
     )
