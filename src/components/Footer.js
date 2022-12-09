914eae638df87929f4285f272aa6a7d3b092d46f
Lee Ryan <2solimanleeryan@gmail.com>
2022-12-09 09:11:51 +0800

    Add Footer component structure

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <h3>Made by Lee, with <span>&#60;3</span></h3>
+        </footer>
+    )
+}
+
+export default Footer;
